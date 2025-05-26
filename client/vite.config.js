36ca115bc065e71ecd2d16d3f8734550d7ffe1b9
Jonus Green <jonusashaba@gmail.com>
2025-05-26 19:10:00 +0300

    vite cofig

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://real-estate-webapp-client.onrender.com",
         secure: false,
         changeOrigin: true,
         ws: true,
