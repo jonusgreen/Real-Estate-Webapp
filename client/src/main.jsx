51e72c2de5dcb5021927edc95c12f4c8a807997f
Jonus Green <jonusashaba@gmail.com>
2025-05-26 18:47:11 +0300

    connection for api

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,10 @@ import './index.css';
 import { persistor, store } from './redux/store.js';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-
+import axios from 'axios';
+  axios.get('https://real-estate-webapp-client.onrender.com')
+      .then(response => console.log(response.data))
+      .catch(error => console.error('There was an error!', error));
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
