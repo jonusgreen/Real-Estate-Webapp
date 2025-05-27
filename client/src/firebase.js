53635abb9f48bc84c21629b7e8d21e97f5ff40ba
Jonus Green <jonusashaba@gmail.com>
2025-05-27 21:21:12 +0300

    firebase updated

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +10,8 @@ const firebaseConfig = {
   projectId: "real-estate-8b3a6",
   storageBucket: "real-estate-8b3a6.appspot.com",
   messagingSenderId: "763543249582",
-  appId: "1:763543249582:web:a7ca00d13129353498433f"
-};
+  appId: "1:763543249582:web:a7ca00d13129353498433f",
+}
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig)
